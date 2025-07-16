39c5249dcbc681fdace0471ae60605113ff87e9d
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-07-16 13:27:12 +0300

    task implementation

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,52 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
+
+router = APIRouter(
+    prefix="/movies",
+)
 
 
-router = APIRouter()
+@router.get("/", response_model=MovieListResponseSchema)
+async def read_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession=Depends(get_db)
+):
+    offset = (page - 1) * per_page
+    query = select(MovieModel).offset(offset).limit(per_page)
+    result = await db.execute(query)
+    movies = result.scalars().all()
 
-# Write your code here
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_query = await db.execute(select(func.count()).select_from(MovieModel))
+    total_items = total_query.scalar()
+    total_pages = int(total_items / per_page)
+
+    prev_page = f"{router.prefix}/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{router.prefix}/?page={page + 1}&per_page={per_page}" if page <= total_pages else None
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items
+    }
+
+
+@router.get("/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession=Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id==movie_id))
+    movie = result.scalar_one_or_none()
+    if movie is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+    return movie
